63d98ea1bca841ce1bb4cc3ce83d230c8a1f8dcd
Bo Li <bli28@mgh.harvard.edu>
2020-03-17 14:55:12 -0400

    Added one README for cylib

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 from setuptools.extension import Extension
 from Cython.Build import cythonize
 from codecs import open
-from os import path
+import os
 
 
-here = path.abspath(path.dirname(__file__))
-with open(path.join(here, "README.rst"), encoding="utf-8") as f:
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 requires = [
