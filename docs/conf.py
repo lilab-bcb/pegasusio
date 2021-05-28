fe8498907168a26dbc872a5a0675cccbabd5de00
Yiming Yang <yang.yihming@gmail.com>
2021-05-28 00:32:41 -0400

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ author = (
 # The short X.Y version
 version = "0.2"
 # The full version, including alpha/beta/rc tags
-release = "0.2.11"
+release = "0.2.12"
 
 
 # -- General configuration ---------------------------------------------------
